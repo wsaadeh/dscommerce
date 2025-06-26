03f18c67643c88bc7699b295edf36e95662c8d3b
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-06-26 11:18:57 -0300

    Add method getRoles in User Class

--- src/main/java/com/saadeh/dscommerce/entities/User.java
+++ src/main/java/com/saadeh/dscommerce/entities/User.java
@@ -124,6 +124,10 @@ public class User implements UserDetails {
         this.password = password;
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
     public void addRole(Role role){ roles.add(role);}
 
     public boolean hasRole(String roleName){
